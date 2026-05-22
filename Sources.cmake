bc795a9eabb9aca41ba9deae298f91e0d5e9ba25
O01eg <397177+o01eg@users.noreply.github.com>
2026-05-23 00:00:48 +0400

    Update boost version to 1.91

--- Sources.cmake
+++ Sources.cmake
@@ -55,6 +55,6 @@ set(GLEW_VERSION 2.3.0)
 set(GLEW_URL https://downloads.sourceforge.net/project/glew/glew/${GLEW_VERSION}/glew-${GLEW_VERSION}.tgz)
 set(GLEW_MD5 1b321240e1f6651175e953b46bccb3f2)
 
-set(BOOST_VERSION 1.90.0)
+set(BOOST_VERSION 1.91.0-1)
 set(BOOST_URL https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.gz)
-set(BOOST_MD5 fdf8c1b6561d0847f4a7c0a488a26906)
+set(BOOST_MD5 0c78ab0978a1df75e956e32024777afe)
